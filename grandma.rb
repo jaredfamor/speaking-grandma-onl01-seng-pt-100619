6daa9dfd9171309888c6672517eedc241053816d
William Famor <jaredfamor@gmail.com>
2019-10-11 15:29:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(speak)
  if speak == speak.upcase
    return "I LOVE YOU TOO PUMPKIN!"
- elsif speak != speak.downcase
-  return "HUH?! SPEAK UP, SONNY!"
+ elsif speak == speak.downcase
+  return "NO, NOT SINCE 1938!"
  else
-   return "NO, NOT SINCE 1938!"
+   return "HUH?! SPEAK UP, SONNY"
    end
 end
